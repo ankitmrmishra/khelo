e26fa3cddc024bb7c383dea4b9b2428e440289fc
ankitmrmishra <ankitmrmishra1118@gmail.com>
2024-12-19 02:47:31 +0530

    added the loader and form for the prediction app

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -2,7 +2,13 @@
 import { Book, ChartNoAxesCombined, Dices, PanelsTopLeft } from "lucide-react";
 import React from "react";
 import { Button } from "./button";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 import {
   Sheet,
   SheetClose,
@@ -11,8 +17,11 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Link from "next/link";
+import { Skeleton } from "./skeleton";
 
 const Navbar = () => {
+  const { isLoaded } = useUser();
+
   return (
     <div className="flex justify-between align-middle items-center border-2 rounded-full p-4 px-10 w-full ml-2 mr-2 md:max-w-max bg-black left-[28%] gap-14 mt-5">
       <div className="flex justify-between align-middle items-center gap-14">
@@ -34,14 +43,22 @@ const Navbar = () => {
       <div className="endCTA gap-5  md:flex hidden">
         <Button>Download App</Button>
         <Button>Trade Online</Button>
-        <SignedOut>
-          <Button className="">
-            <SignInButton />
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        {!isLoaded ? (
+          <div className="">
+            <Skeleton className="h-12 w-12 rounded-full" />
+          </div>
+        ) : (
+          <div className="">
+            <SignedOut>
+              <Button className="">
+                <SignInButton />
+              </Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        )}
       </div>
 
       <div className="md:hidden flex  justify-center align-middle items-center gap-2">
